89d9ec7a24ea9e44c1129992f960f97382043c88
Dan Hosi <danh4160@outlook.com>
2023-12-16 17:59:48 -0500

    css formatting

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Header from "@/components/navbar"
+import Introduction from '@/components/introduction'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,13 +19,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="bg-white text-white relative h-[5000px]">
+      <body className="bg-white text-white relative h-[5000px] pt-32">
         <div 
-          className="absolute top-[2rem] w-[30rem] h-[30rem] left-[40rem] z-10 rounded-full blur-[10rem] bg-blue-300 ">
+          className="absolute top-[2rem] w-[30rem] h-[30rem] left-[40rem] z-100 rounded-full blur-[10rem] bg-blue-300 ">
         </div>
 
         <div 
-          className="absolute top-[2rem] w-[30rem] h-[30rem] right-[40rem] z-10 rounded-full blur-[10rem] bg-red-300 ">
+          className="absolute top-[2rem] w-[30rem] h-[30rem] right-[40rem] z-100 rounded-full blur-[10rem] bg-red-300 ">
         </div>
         <Header />
         {children}
