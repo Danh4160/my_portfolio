0128f5614e0ed8dc15c06846023e790cdfb877ed
DanHosi <danh4160@outlook.com>
2023-12-17 16:21:45 -0500

    section divider component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Introduction from "@/components/introduction"
+import Divider from "@/components/divider"
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center">
+    <main className="flex flex-col items-center px-4">
       <Introduction />
+      <Divider />
     </main>
   )
 }
