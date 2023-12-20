1f2293615ab2ec3a5f9f54e6c82663d28c3a6154
DanHosi <danh4160@outlook.com>
2023-12-20 14:19:32 -0500

    css update

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="!scroll-smooth">
-      <body className="bg-white text-white relative h-[5000px] pt-32">
+      <body className="bg-gray-50 text-gray-950 relative h-[5000px] pt-32">
         <div 
           className="absolute top-[2rem] w-[30rem] h-[30rem] left-[40rem] z-100 rounded-full blur-[10rem] bg-blue-300">
         </div>
