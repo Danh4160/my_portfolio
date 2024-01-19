e8d319b8dd79d8fe84fb248d9f93c8b87f58c488
DanHosi <danh4160@outlook.com>
2024-01-18 23:02:29 -0500

    light mode bug fix

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -11,7 +11,7 @@ const Header = () => {
   return ( 
     <header className="z-[999] relative">
         <motion.div 
-        className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75"
+        className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-opacity-80 bg-gray-950 border-black/40 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full"
             initial={{ y: -100, x: "-50%", opacity: 0 }}
             animate={{ y: 0, x: "-50%", opacity: 1}}
             >
@@ -31,8 +31,8 @@ const Header = () => {
                   setActiveSection(link.name)}
                 }
                 className=                
-                  {activeSection === link.name ? "flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-black dark:hover:text-gray-400" :
-                    "flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-white dark:hover:text-gray-300"}
+                  {activeSection === link.name ? "flex w-full items-center justify-center px-3 py-3 transition text-black hover:text-gray-400" :
+                    "flex w-full items-center justify-center px-3 py-3 hover:text-gray-400 transition text-gray-250"}
                 href={link.hash}
               >
                 {activeSection === link.name && (
